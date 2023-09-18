92483de565f7f2617b5b64083249ccfbfba32aa4
Marco Antônio Gusmão Carvalho <marcocarvalho.web@gmail.com>
2023-09-18 14:58:13 +0000

    Classe Pesonagem

--- Personagem.cs
+++ Personagem.cs
@@ -1,8 +1,18 @@
 public class Personagem {
     private string _tipo;
+    private int _posX;
+    private int _posY;
 
     public string Tipo {
         get {return this._tipo;}
         set {this._tipo = value;}
+    }
+    public int PosX {
+        get {return this._posX;}
+        set {this._posX = value;}
+    }
+    public int PosY {
+        get {return this._posY;}
+        set {this._posY = value;}
     }
 }
